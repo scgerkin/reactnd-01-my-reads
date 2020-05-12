b7d4a5295d265e4fd97ec8a57a7a2f172ba2c2b4
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-05-12 09:26:27 -0400

    Displaying book information!

--- src/components/bookshelf/BookItem.js
+++ src/components/bookshelf/BookItem.js
@@ -1,10 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
+import BookShelfChanger from "./BookShelfChanger";
 
 class BookItem extends React.Component {
+  static propTypes = {
+    book: PropTypes.object.isRequired,
+    onChangeShelf: PropTypes.func.isRequired
+  }
+
   render() {
+    const { book, onChangeShelf } = this.props;
+
     return (
         <div className={"book"}>
-
+          <div
+              className={"book-cover"}
+              style={{
+                width: 128,
+                height: 193,
+                backgroundImage: `url(${book.imageLinks.thumbnail})`
+              }}
+          />
+          <BookShelfChanger onChangeShelf={onChangeShelf}/>
+          <div className={"book-title"}>{book.title}</div>
+          <div className={"book-authors"}>{book.authors[0]}</div>
         </div>
     )
   }
--- src/components/bookshelf/BookShelfChanger.js
+++ src/components/bookshelf/BookShelfChanger.js
@@ -1,10 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class BookShelfChanger extends React.Component {
+  static propTypes = {
+    onChangeShelf: PropTypes.func.isRequired
+  }
   render() {
+    const { onChangeShelf } = this.props;
     return (
         <div className={"book-shelf-changer"}>
-
+          <select onChange={onChangeShelf}>
+            <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
     )
   }
