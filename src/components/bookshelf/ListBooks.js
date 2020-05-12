7ac1d868ba1ef86df47ff7c94c4db0c8eb2a6775
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-05-11 21:16:21 -0400

    Set up proptypes for ListBooks

--- src/components/bookshelf/ListBooks.js
+++ src/components/bookshelf/ListBooks.js
@@ -1,6 +1,11 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 class ListBooks extends React.Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+  }
+
   render() {
     return (
         <div className={"list-books"}>
