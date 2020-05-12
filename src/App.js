fb753e1da5124e4228948e9a66ab3cf454093aa6
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-05-12 11:29:52 -0400

    Get books from backend API

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
 import './App.css'
-
+import * as BooksAPI from "./utils/BooksAPI";
 import ListBooks from "./components/bookshelf/ListBooks";
-
-const fooBooks = [
-  {
-    id: 1,
-    title: "foo",
-    authors: ["bar"],
-    imageLinks: {thumbnail: "http://placekitten.com/50/50"},
-    shelf: "currentlyReading"
-  },
-  {
-    id: 2,
-    title: "baz",
-    authors: ["weeble"],
-    imageLinks: {thumbnail: "http://placekitten.com/50/50"},
-    shelf: "currentlyReading"
-  },
-  {
-    id: 3,
-    title: "bum",
-    authors: ["bax"],
-    imageLinks: {thumbnail: "http://placekitten.com/50/50"},
-    shelf: "read"
-  }
-]
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState(() => ({
+        books
+      }))
+    })
   }
 
   changeShelf(shelf) {
@@ -47,7 +24,7 @@ class BooksApp extends React.Component {
     return (
         <div className="app">
           <ListBooks
-              books={fooBooks}
+              books={this.state.books}
               changeShelf={this.changeShelf}/>
         </div>
     )
@@ -55,5 +32,3 @@ class BooksApp extends React.Component {
 }
 
 export default BooksApp
-
-//{{title: "foo", authors: ["bar"], imageLinks: {thumbnail: "http://placekitten.com/50/50"}}
